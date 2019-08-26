017baa79579b1444882b6895d087af070ccdecc9
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-26 16:39:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else
+      false 
+    end
   end
 
   def question?
-
+  if self.end_with?("?")
+      true 
+    else
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else
+      false 
+    end
   end
 
   def count_sentences
-
+   sentence_count = self.split(/\.|\?|\!/).delete_if {|n| n.length <= 1}
+   return sentence_count.length
+   binding.pry
   end
 end
